4bcc00d24fd36e9851e5106974f0de9739a965bf
traviscancode604 <traviscancode604@gmail.com>
2021-11-03 19:28:01 -0700

    Adding linter and repeat play.

--- js101_loan_calculator/loan_calc.js
+++ js101_loan_calculator/loan_calc.js
@@ -4,18 +4,24 @@ let inputLoanAmount;
 let inputAnnualPercentageRate;
 let inputLoanDurationInYears;
 
-function amortLoanPayment(loanAmount, annualPercentageRate, loanDurationInYears) {
+function amortLoanPayment(
+  loanAmount, annualPercentageRate, loanDurationInYears
+) {
   let monthlyInterestRate = Number(annualPercentageRate) / 12;
   let loanDurationInMonths = Number(loanDurationInYears) * 12;
   let monthlyPayment;
 
   if (Number(annualPercentageRate)) {
-    monthlyPayment = (Number(loanAmount) * (monthlyInterestRate / 
-    (1 - Math.pow((1 + monthlyInterestRate), (-loanDurationInMonths))))).toFixed(2);
+    monthlyPayment = (
+      Number(loanAmount)
+      * (monthlyInterestRate
+      / (1 - Math.pow((1 + monthlyInterestRate),
+        (-loanDurationInMonths))))
+    ).toFixed(2);
   } else {
     monthlyPayment = (Number(loanAmount) / loanDurationInMonths).toFixed(2);
   }
-return monthlyPayment;
+  return monthlyPayment;
 }
 
 function prompt(message) {
@@ -26,43 +32,59 @@ function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
-console.log("***********************************************")
+console.log("***********************************************");
 console.log("*** Welcome to the Loan Calculator Program! ***");
-console.log("***********************************************\n")
+console.log("***********************************************\n");
 
-prompt(MESSAGES.principalPrompt);
-inputLoanAmount = readline.question();
-
-while (inputLoanAmount === '0' || invalidNumber(inputLoanAmount)) {
-  prompt(MESSAGES.inputErrorMsg);
+while (true) {
+  prompt(MESSAGES.principalPrompt);
   inputLoanAmount = readline.question();
-}
 
-prompt(MESSAGES.annualInterestPrompt);
-inputAnnualPercentageRate = readline.question();
+  while (inputLoanAmount === '0' || invalidNumber(inputLoanAmount)) {
+    prompt(MESSAGES.inputErrorMsg);
+    inputLoanAmount = readline.question();
+  }
 
-while (invalidNumber(inputAnnualPercentageRate)) {
-  prompt(MESSAGES.inputErrorMsg);
+  prompt(MESSAGES.annualInterestPrompt);
   inputAnnualPercentageRate = readline.question();
-}
 
-prompt(MESSAGES.loanDurationPrompt);
-inputLoanDurationInYears = readline.question();
+  while (invalidNumber(inputAnnualPercentageRate)) {
+    prompt(MESSAGES.inputErrorMsg);
+    inputAnnualPercentageRate = readline.question();
+  }
 
-while (inputLoanDurationInYears < 0.5 || invalidNumber(inputLoanDurationInYears)) {
-  // Need 7 sig-figs to properly calculate months.
-  // So we'll use a reasonable loan periods (ie: half a year or more).
-  prompt(MESSAGES.inputErrorMsg);
+  prompt(MESSAGES.loanDurationPrompt);
   inputLoanDurationInYears = readline.question();
+
+  while (
+    inputLoanDurationInYears < 0.5 || invalidNumber(inputLoanDurationInYears)
+  ) {
+    // Need 7 sig-figs to properly calculate months.
+    // So we'll use a reasonable loan periods (ie: half a year or more).
+    prompt(MESSAGES.inputErrorMsg);
+    inputLoanDurationInYears = readline.question();
+  }
+
+  prompt(
+    MESSAGES.outputMsg
+    + amortLoanPayment(
+      inputLoanAmount,
+      inputAnnualPercentageRate,
+      inputLoanDurationInYears
+    )
+  );
+
+  prompt(MESSAGES.retryMsg);
+  let answer = readline.question();
+  if (answer[0].toLowerCase() !== 'y') break;
 }
-
-prompt(MESSAGES.outputMsg + amortLoanPayment(inputLoanAmount, inputAnnualPercentageRate, inputLoanDurationInYears));
-
-console.log(amortLoanPayment(inputLoanAmount, inputAnnualPercentageRate, inputLoanDurationInYears));
-
+prompt(MESSAGES.exitMsg);
 /*
 console.log(amortLoanPayment(10000, 0.05, 0.5)); // $1,691.06
 console.log(amortLoanPayment(100000, 0.05, 10)); // $1060.66
 console.log(amortLoanPayment(100000, 0.05, 5)); // $1887.12
-console.log(amortLoanPayment(100000, 0.0, 5)); // $1666.67. 0 is an edge case (you end up with a 0 in denominator)
+console.log(amortLoanPayment(100000, 0.0, 5)); // $1666.67
+
+0% interest is an edge case (you end up with a 0 in denominator)
+
 */
