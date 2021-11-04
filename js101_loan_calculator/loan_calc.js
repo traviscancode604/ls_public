a7de93a4db75ff1cabc09880925e876cc3b388b4
traviscancode604 <traviscancode604@gmail.com>
2021-11-03 18:30:34 -0700

    Adding skeleton of program. Data validation works for edge cases. Does not loop through yet.

--- js101_loan_calculator/loan_calc.js
+++ js101_loan_calculator/loan_calc.js
@@ -5,15 +5,15 @@ let inputAnnualPercentageRate;
 let inputLoanDurationInYears;
 
 function amortLoanPayment(loanAmount, annualPercentageRate, loanDurationInYears) {
-  let monthlyInterestRate = annualPercentageRate / 12;
-  let loanDurationInMonths = loanDurationInYears * 12;
+  let monthlyInterestRate = Number(annualPercentageRate) / 12;
+  let loanDurationInMonths = Number(loanDurationInYears) * 12;
   let monthlyPayment;
 
-  if (annualPercentageRate) {
-    monthlyPayment = (loanAmount * (monthlyInterestRate / 
+  if (Number(annualPercentageRate)) {
+    monthlyPayment = (Number(loanAmount) * (monthlyInterestRate / 
     (1 - Math.pow((1 + monthlyInterestRate), (-loanDurationInMonths))))).toFixed(2);
   } else {
-    monthlyPayment = (loanAmount / loanDurationInMonths).toFixed(2);
+    monthlyPayment = (Number(loanAmount) / loanDurationInMonths).toFixed(2);
   }
 return monthlyPayment;
 }
@@ -22,14 +22,47 @@ function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-prompt("Welcome to the Loan Calculator Program!");
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+console.log("***********************************************")
+console.log("*** Welcome to the Loan Calculator Program! ***");
+console.log("***********************************************\n")
 
 prompt(MESSAGES.principalPrompt);
 inputLoanAmount = readline.question();
 
+while (inputLoanAmount === '0' || invalidNumber(inputLoanAmount)) {
+  prompt(MESSAGES.inputErrorMsg);
+  inputLoanAmount = readline.question();
+}
 
-prompt(MESSAGES.testMsg);
-console.log(inputLoanAmount);
+prompt(MESSAGES.annualInterestPrompt);
+inputAnnualPercentageRate = readline.question();
+
+while (invalidNumber(inputAnnualPercentageRate)) {
+  prompt(MESSAGES.inputErrorMsg);
+  inputAnnualPercentageRate = readline.question();
+}
+
+prompt(MESSAGES.loanDurationPrompt);
+inputLoanDurationInYears = readline.question();
+
+while (inputLoanDurationInYears < 0.5 || invalidNumber(inputLoanDurationInYears)) {
+  // Need 7 sig-figs to properly calculate months.
+  // So we'll use a reasonable loan periods (ie: half a year or more).
+  prompt(MESSAGES.inputErrorMsg);
+  inputLoanDurationInYears = readline.question();
+}
+
+prompt(MESSAGES.outputMsg + amortLoanPayment(inputLoanAmount, inputAnnualPercentageRate, inputLoanDurationInYears));
+
+console.log(amortLoanPayment(inputLoanAmount, inputAnnualPercentageRate, inputLoanDurationInYears));
+
+/*
+console.log(amortLoanPayment(10000, 0.05, 0.5)); // $1,691.06
 console.log(amortLoanPayment(100000, 0.05, 10)); // $1060.66
 console.log(amortLoanPayment(100000, 0.05, 5)); // $1887.12
 console.log(amortLoanPayment(100000, 0.0, 5)); // $1666.67. 0 is an edge case (you end up with a 0 in denominator)
+*/
